2ad72222b6e288bc5f1afa77708fb0a906e58d0a
RobertoHortaFilho <robertohortafilho@gmail.com>
2022-05-25 05:22:13 -0300

    width height ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const board = document.querySelector(".board")
 let colorInvert = true
 for (let  i = 0; i<64; i++){
     const slot = document.createElement('div')
-    slot.className = 'slot'
+    slot.className = 'slot horse'
     slot.style.backgroundColor = colorInvert ? '#746A9C' : '#E8D5C5';
     if (i % 8 != 7) colorInvert = !colorInvert
     board.appendChild(slot)
