a1a44215c72c11a0c21f8df0f246d76e07b6809e
RobertoHortaFilho <robertohortafilho@gmail.com>
2022-05-25 05:06:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const board = document.querySelector(".board")
+
+
+let colorInvert = true
+for (let  i = 0; i<64; i++){
+    const slot = document.createElement('div')
+    slot.className = 'slot'
+    slot.style.backgroundColor = colorInvert ? '#746A9C' : '#E8D5C5';
+    if (i % 8 != 7) colorInvert = !colorInvert
+    board.appendChild(slot)
+}
